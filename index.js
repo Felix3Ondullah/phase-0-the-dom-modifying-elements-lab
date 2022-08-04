d54b6f3e805600f2ebe7e5cec2d531069678bdc9
Felix Ondullah <felixondullah@gmail.com>
2022-08-04 15:35:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element = document.querySelector( 'main' );
+element.remove();
+const newHeader = document.createElement( 'h1' );
+newHeader.id = "victory";
+newHeader.innerHTML = "Felix Ondullah is the champion";
